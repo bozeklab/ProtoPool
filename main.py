6c2644db1f42170e8872f82638698307caacce49
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-10-04 13:27:31 +0200

    resnet path

--- main.py
+++ main.py
@@ -668,7 +668,7 @@ def learn_model(opt: Optional[List[str]]) -> None:
 
     # ===================fine tune=====================
 
-    for h in heaps:
+    for hidx, h in enumerate(heaps):
         assert len(h) == 5
         for k in h:
             img = h[k].patch
@@ -1141,7 +1141,7 @@ def update_prototypes_on_batch_heaps(search_batch_input, start_index_of_search_b
             mask_tensor = transforms.ToTensor()(mask)
             mask_tensor = mask_tensor[:, proto_bound_j[0]: proto_bound_j[1], proto_bound_j[2]: proto_bound_j[3]]
 
-            he = HeapPatch(distance=-heap_dist, patch=proto_img_j, mask_path=mask_tensor, filename=filename_j)
+            he = HeapPatch(distance=-heap_dist, patch=proto_img_j, mask_patch=mask_tensor, filename=filename_j)
             if len(heaps[j]) < 5:
                 heapq.heappush(heaps[j], he)
             else:
